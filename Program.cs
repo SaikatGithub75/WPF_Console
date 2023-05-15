971ef8c1cdc1c0d25f0a0846d1ee8f8ed7e1df15
Saikat_Das <Saikat_Das@EPINHYDW085A>
2023-05-15 18:13:24 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Form
+using System.Drawing.Drawing2D;
+
+
+namespace WPF_Console
+{
+    public partial class Program: Form
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("This is a '.NET Core – console!'");
+            Console.ReadLine();
+        }
+    }
+}
